e8ce65212724050e8134bb8a47588948834e3a6c
yuyingsu <yuyingsujob@gmai.com>
2021-01-12 11:13:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Form from "./Form";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    state = {
+      firstName : "",
+      lastName : "",
+      showName : true,
+      showForm : false,
+      prevFirstName: "",
+      prevLastName: ""
+    }
+
+    change = () => {
+      this.setState({showForm:!this.state.showForm, showName:!this.state.showName});
+    }
+
+    changeName = (event) => {
+      this.setState({showForm:!this.state.showForm, showName:!this.state.showName,
+        prevFirstName: this.state.firstName, prevLastName: this.state.lastName});
+    }
+
+    changeFName= (e) => {
+      this.setState({firstName: e.target.value});
+    }
+
+    changeLName= (e) => {
+      this.setState({lastName: e.target.value});
+    }
+
+    rollBack = () => {
+      this.setState({showForm:!this.state.showForm, showName:!this.state.showName, 
+        firstName: this.state.prevFirstName, lastName: this.state.prevLastName});
+    }
+
+    render(){
+
+      let name = null;
+
+      if (this.state.showName) {
+        name = (
+          <div>
+          <div>
+          {this.state.firstName}
+          </div>
+          <div>
+          {this.state.lastName}
+          </div>
+          <button onClick={this.change}>Edit</button>
+          </div>
+        );
+      }
+
+      return(
+              <div className="App">
+              <header className="App-header">
+              {name}
+              {this.state.showForm ? (
+              <Form changeName={this.changeName} rollBack={this.rollBack} 
+                    changeFName={this.changeFName} changeLName={this.changeLName}
+                    fName={this.state.firstName} lName={this.state.lastName}/>
+              ) : null}
+              </header>
+              </div>
+
+        )};
+
 }
 
 export default App;
